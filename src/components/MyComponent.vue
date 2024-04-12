ec7a74f5ac1d7e0559548591d30f725378806da1
Ryan Jeynes <ryanjeynes@outlook.com>
2024-04-12 16:31:21 +0100

    Add custom component with data binding

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <h2>Email: {{ email }}</h2>
+  <h2>Username: {{ username }}</h2>
+  <h2>Account Status: {{ getStatus() }}</h2>
+</template>
+
+<script>
+export default {
+  name: 'MyComponent',
+  data() {
+    return {
+      username: 'Tester',
+      email: 'test@tester.com',
+      getStatus: function () {
+        return 'Unverified'
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+h2 {
+  color: hsla(160, 100%, 37%, 1);
+}
+</style>
