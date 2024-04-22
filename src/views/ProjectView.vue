a78229e6b0a6c143e8be02e1b9e9b57334a8325b
Ryan Jeynes <ryanjeynes@outlook.com>
2024-04-22 22:11:33 +0100

    View for final project

---
+++ src/views/ProjectView.vue
@@ -0,0 +1,28 @@
+<script setup>
+import ProjectSignUp from '../components/final_project/SignUpForm.vue'
+import ProjectProfile from '../components/final_project/ProjectProfile.vue'
+</script>
+
+<template>
+  <h1>Final Project</h1>
+  <h2>Finishing up the course with an API project</h2>
+  <!--<ProjectSignUp />-->
+  <component :is="active" />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      active: 'ProjectSignUp'
+    }
+  },
+  components: { ProjectSignUp, ProjectProfile },
+  beforeMount() {
+    const isSignedUp = localStorage.getItem('user-info')
+    if (isSignedUp) {
+      this.active = 'ProjectProfile'
+    }
+  }
+}
+</script>
